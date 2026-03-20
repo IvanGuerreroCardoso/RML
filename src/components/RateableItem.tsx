7914a50fa4c046583567c5e7aef7a06717233101
navi <ivgrocar@gmail.com>
2026-03-20 11:08:28 -0600

    rating update to slider and minor adjustments

--- src/components/RateableItem.tsx
+++ src/components/RateableItem.tsx
@@ -1,11 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
-import { ListItem, Button, Slider } from "@rneui/base";
+import { ListItem, Button, Slider, Text } from "@rneui/base";
 import { Overlay } from "@rneui/themed";
 import { Feather } from "@react-native-vector-icons/feather"
 import { ListItem as Item } from "../models/models";
 import { deleteListItem, updateListItem } from "../services/listItemsDbService.ts";
 import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 interface RatableListProps{
     item: Item,
@@ -15,10 +15,18 @@ interface RatableListProps{
 const styles = StyleSheet.create({
     overlay: {
         backgroundColor: "#fff",
+    },
+    modalView: {
+        minWidth: 200
+    },
+    rateText:{
+        alignSelf: "center",
+        marginBottom: 10
     }
 })
 
 export default function RateableItem(props: RatableListProps) {
+    console.log("props", props)
     const navigation = useNavigation();
     const [rateVisible, setRateVisible] = useState(false);
     const [rateChanged, setRateChanged] = useState(false);
@@ -34,7 +42,10 @@ export default function RateableItem(props: RatableListProps) {
     };
 
     function rateAndSave(){
-        updateListItem({...props.item, rate: rate, checked: true}).then(()=>props.updateList());
+        updateListItem({...props.item, rate: rate, checked: true}).then(()=>{
+            props.updateList();
+            setRateVisible(false);
+        });
     }
 
     function deleteItem(){
@@ -79,7 +90,13 @@ export default function RateableItem(props: RatableListProps) {
                 */
                 >        
                 <ListItem.Content>
-                    <ListItem.Title>{props.item.name}{props.item.checked ? ' Finished' : ''}</ListItem.Title>
+                    
+                    <ListItem.Title>{props.item.checked === true && 
+                            <Feather
+                            name="check-square"
+                            size={20}
+                            color="#26a50d"
+                            />}{props.item.name}</ListItem.Title>
                     <ListItem.Subtitle>{props.item.author?.name} | {year}{props.item.rate ? ` | ${props.item.rate}/10` : ""}</ListItem.Subtitle>
                 </ListItem.Content>
                 <Feather name="chevron-right" size={20} color="#000" />
@@ -88,27 +105,29 @@ export default function RateableItem(props: RatableListProps) {
                 isVisible={rateVisible} 
                 onBackdropPress={()=>setRateVisible(false)}
                 style={styles.overlay}>
-                <Slider
-                    value={rate}
-                    onValueChange={ratingCompleted}
-                    maximumValue={10}
-                    minimumValue={0}
-                    step={1}
-                    allowTouchTrack
-                    trackStyle={{ height: 5, backgroundColor: 'transparent' }}
-                    thumbStyle={{ height: 20, width: 20, backgroundColor: 'transparent' }}
-                    thumbProps={{
-                    children: (
-                        <Feather
-                        name="heart"
-                        size={20}
-                        color="#ee5997"
-                        />
-                    ),
-                    }}
-                />
-
-                <Button onPress={rateAndSave} disabled={rateChanged} >Ok</Button>
+                <View style={styles.modalView}>
+                    <Slider
+                        value={rate}
+                        onValueChange={ratingCompleted}
+                        maximumValue={10}
+                        minimumValue={0}
+                        step={1}
+                        allowTouchTrack
+                        trackStyle={{ height: 7, backgroundColor: 'transparent' }}
+                        thumbStyle={{ height: 30, width: 30, backgroundColor: 'transparent' }}
+                        thumbProps={{
+                        children: (
+                            <Feather
+                            name="heart"
+                            size={30}
+                            color="#ee5997"
+                            />
+                        ),
+                        }}
+                    />
+                    <Text style={styles.rateText}>{rate} / 10</Text>
+                    <Button onPress={rateAndSave} disabled={!rateChanged} >Ok</Button>
+                </View>                
             </Overlay>
         </>
     );
